cd8dbe76ba3fca31d3a7cdb5ee2a411d4418f40e
GAURAV KATARA <gauravkatara53@gmail.com>
2026-03-16 19:52:31 +0530

    add

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
-        <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+        <NextThemesProvider attribute="class" defaultTheme="light" enableSystem={false} forcedTheme="light">
             {children}
         </NextThemesProvider>
     );
