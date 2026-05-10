9b68e2e2bfda3fce6896a5e967ca01762f31fd28
GAURAV KATARA <gauravkatara53@gmail.com>
2026-05-10 13:46:12 +0530

    Add

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
